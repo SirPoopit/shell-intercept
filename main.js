38028fd283515d1f64e00566ea0ddd616db61b76
SirPoopit <sirpoopit@gmail.com>
2024-08-29 17:02:15 -0500

    first attept

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();import * as THREE from 'three';
