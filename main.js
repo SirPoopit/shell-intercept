d7797c6219a105156b913c4dda4ccc376907a785
SirPoopit <sirpoopit@gmail.com>
2024-08-29 17:23:37 -0500

    A

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -20,8 +16,10 @@ scene.add( cube );
 camera.position.z = 5;
 
 function animate() {
+
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
 	renderer.render( scene, camera );
+
 }
-renderer.setAnimationLoop( animate );
